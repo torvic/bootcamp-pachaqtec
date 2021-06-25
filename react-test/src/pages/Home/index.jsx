a6265074643f381b4c5922463de0c5c8680c19db
torvic <130111@unsaac.edu.pe>
2021-06-24 21:50:13 -0500

    axios

--- react-test/src/pages/Home/index.jsx
+++ react-test/src/pages/Home/index.jsx
@@ -1,36 +1,93 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-	const [count, setCount] = useState(0);
+	const [data, setData] = useState(null);
+  const [nombreMascota, setNombreMascota] = useState("");
+  const [tipoMascota, setTipoMascota] = useState("");
 
-	const handleAdd = () => {
-		setCount(count + 1);
+	useEffect(() => {
+		getData();
+	}, [])
+
+	const getData = async () => {
+		try {
+			let res = await axios.get("http://localhost:5000/mascotas");
+			let data = await res.data;
+			console.log(data);
+			setData(data);
+		} catch (error) {
+			
+		}
 	}
 
-	const handleRemove = () => {
-		setCount(count - 1);
-	}
-	return (
-		<div className="home">
-			<h1>Home</h1>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/login" >Login</Link>
-					</li>
-					<li>
-						<Link to="/users" >Users</Link>
-					</li>
-				</ul>
-			</nav>
 
-			<h2>{count}</h2>
-			<button onClick={handleRemove}>-</button>
-			<button onClick={handleAdd}>+</button>
-		</div>
-	)
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let isValid = true;
+    if (nombreMascota === "") {
+      isValid = false;
+    }
+    if (tipoMascota === "") {
+      isValid = false;
+    }
 
-export default Home 
+    if (!isValid) return;
+
+    try {
+      const response = await axios.post("http://localhost:5000/mascotas", {
+        id: Math.random() * 100,
+        nombre: nombreMascota,
+        tipo: tipoMascota,
+      });
+      console.log("response", response);
+      setNombreMascota("");
+      setTipoMascota("");
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <h2>Agregar mascotas</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={nombreMascota}
+          onChange={(e) => setNombreMascota(e.target.value)}
+          placeholder="Nombre de mascota"
+        />
+        <br />
+        <input
+          type="text"
+          value={tipoMascota}
+          onChange={(e) => setTipoMascota(e.target.value)}
+          placeholder="Tipo de mascota"
+        />
+        <br />
+        <input type="submit" value="Registrar mascota" />
+      </form>
+			<h3>Lista de Mascotas</h3>
+			<div>
+				<ol>
+					{data && data.map(el => <li key={el.id} >{el.nombre} - {el.tipo}</li> )}
+				</ol>
+			</div>
+    </div>
+  );
+};
+
+export default Home;
