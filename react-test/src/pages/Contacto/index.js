52e763670a47cda306f27f8b5fc0a7901d68db6d
torvic <130111@unsaac.edu.pe>
2021-07-05 22:59:37 -0500

    Ant design

--- react-test/src/pages/Contacto/index.js
+++ react-test/src/pages/Contacto/index.js
@@ -1,27 +1,118 @@
+/* eslint-disable no-template-curly-in-string */
 import 'antd/dist/antd.css';
+import FormMethod from '../../componentes/FormMethod';
+import FormLayout from '../../componentes/FormLayout';
+import { Form, Input, InputNumber, Button } from 'antd';
 import { DatePicker, Space } from 'antd';
+import { Row, Col } from 'antd';
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 8,
+  },
+};
+
+const validateMessages = {
+  // eslint-disable-next-line no-template-curly-in-string
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
 
 const Contacto = () => {
   const { RangePicker } = DatePicker;
 
-  const onChange = (date, dateString) => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  /* const onChange = (date, dateString) => {
     console.log(date, dateString);
   };
 
   const handleRangePicker = (date, dateString) => {
     console.log(dateString);
-  };
+  }; */
 
   return (
-    <div style={{ marginLeft: '20px' }}>
-      <h2>Formularios de Contacto</h2>
-      <form>
-        <Space direction="vertical">
-          <DatePicker onChange={onChange} />
-          <RangePicker onChange={handleRangePicker} />
-        </Space>
-      </form>
-    </div>
+    <Row align="middle">
+      <Col span={8}>
+        <Form
+          style={{ marginTop: '10%' }}
+          {...layout}
+          name="nest-messages"
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+        >
+          <Form.Item
+            name={['user', 'name']}
+            label="Name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={['user', 'email']}
+            label="Email"
+            rules={[
+              {
+                type: 'email',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={['user', 'age']}
+            label="Age"
+            rules={[
+              {
+                type: 'number',
+                min: 0,
+                max: 99,
+              },
+            ]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item name={['user', 'website']} label="Website">
+            <Input />
+          </Form.Item>
+          <Form.Item name={['user', 'introduction']} label="Introduction">
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+      <Col span={8}>
+        <FormMethod />
+      </Col>
+      <Col span={8}>
+        <FormLayout />
+      </Col>
+      <Col span={8}>
+        <FormLayout />
+      </Col>
+      <Col span={8}>
+        <FormLayout />
+      </Col>
+    </Row>
   );
 };
 
--- react-test/src/pages/Home/index.jsx
+++ react-test/src/pages/Home/index.jsx
@@ -1,6 +1,13 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Row, Col } from 'antd';
+import { Carousel } from 'antd';
+import { Button } from 'antd';
+import { Divider } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 const Home = () => {
   const [data, setData] = useState(null);
@@ -47,51 +54,92 @@ const Home = () => {
   };
 
   return (
-    <div className="home">
-      <h1>Home</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/contacto">Contacto</Link>
-          </li>
-        </ul>
-      </nav>
-      <h2>Agregar mascotas</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={nombreMascota}
-          onChange={(e) => setNombreMascota(e.target.value)}
-          placeholder="Nombre de mascota"
-        />
-        <br />
-        <input
-          type="text"
-          value={tipoMascota}
-          onChange={(e) => setTipoMascota(e.target.value)}
-          placeholder="Tipo de mascota"
-        />
-        <br />
-        <input type="submit" value="Registrar mascota" />
-      </form>
-      <h3>Lista de Mascotas</h3>
-      <div>
-        <ol>
-          {data &&
-            data.map((el) => (
-              <li key={el.id}>
-                {el.nombre} - {el.tipo}
-              </li>
-            ))}
-        </ol>
-      </div>
-    </div>
+    <Layout className="layout" style={{ minHeight: '100vh' }}>
+      <Header style={{ backgroundColor: '#fff' }}>
+        <Row justify="end" gutter={[8, 16]}>
+          <Col>
+            <Button type="primary">Iniciar Sesion</Button>
+          </Col>
+          <Col>
+            <Button type="primary">Registrarse</Button>
+          </Col>
+        </Row>
+      </Header>
+      <Layout>
+        <Content style={{ height: '300px' }}>
+          <Carousel autoplay>
+            <div>
+              <img src="https://placeimg.com/1300/300/tech" alt="tech" />
+            </div>
+            <div>
+              <img src="https://placeimg.com/1300/300/any" alt="any" />
+            </div>
+            <div>
+              <img src="https://placeimg.com/1300/300/animals" alt="animals" />
+            </div>
+          </Carousel>
+        </Content>
+        <Content>
+          <img
+            src="https://placeimg.com/640/480/any"
+            alt="any"
+            style={{ width: '100%', height: '450px' }}
+          />
+        </Content>
+        <Content style={{ height: '450px' }}>
+          <div className="site-layout-content">
+            <div className="home">
+              <h1>Home</h1>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/users">Users</Link>
+                  </li>
+                  <li>
+                    <Link to="/contacto">Contacto</Link>
+                  </li>
+                </ul>
+              </nav>
+              <h2>Agregar mascotas</h2>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={nombreMascota}
+                  onChange={(e) => setNombreMascota(e.target.value)}
+                  placeholder="Nombre de mascota"
+                />
+                <br />
+                <input
+                  type="text"
+                  value={tipoMascota}
+                  onChange={(e) => setTipoMascota(e.target.value)}
+                  placeholder="Tipo de mascota"
+                />
+                <br />
+                <input type="submit" value="Registrar mascota" />
+              </form>
+              <h3>Lista de Mascotas</h3>
+              <div>
+                <ol>
+                  {data &&
+                    data.map((el) => (
+                      <li key={el.id}>
+                        {el.nombre} - {el.tipo}
+                      </li>
+                    ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </Content>
+      </Layout>
+      <Footer style={{ textAlign: 'center', backgroundColor: 'blue' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 };
 
